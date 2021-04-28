3a69b504b98cbbae670c51256e83528bc6616c06
GowthamanaG <gowthaman.gobalasingam@gmail.com>
2021-04-28 21:38:23 +0200

    Update sampling method with using bernoulli distribution for class flipping, add a polynom as decision function

--- datasets.py
+++ datasets.py
@@ -9,21 +9,40 @@ def parabola(x): # parabola
 
 
 def polynom_3(x):
-    return 7.75 * x ** 3 - 14 * x ** 2 + 6.25 * x + 0.25
+    return 2.8* x ** 3 - 6.7 * x ** 2 + 4 * x + 0.4
 
 
-def sample_2D_data(num_samples):
-    samples = np.random.uniform(low=0, high=1.5, size=(num_samples, 2))
-    labels = np.zeros(num_samples) # one-hot encoded
+def sample_2D_data(num_samples, fun, space):
+    vertical_dist_from_fun = lambda x, y: np.abs(y - fun(x))
+    xx, yy = np.meshgrid(np.linspace(0, space[0], 100), np.linspace(0, space[1], 100))
+    # Die Idee: Wahrscheinlichkeit für das Umdrehen für die jeweilige klasse ist größer,
+    # je weiter man weg ist von der Trennlinie (by Fabricio)
+    max_val = np.max(vertical_dist_from_fun(xx, yy))
+    max_flip_prob = 0.5
+    sharpness = 6
+    flip_prob = lambda xx, yy: max_flip_prob * (1 - vertical_dist_from_fun(xx, yy) / max_val) ** 5
+    class_ = lambda x, y: np.sign(polynom_3(x) - y)
 
-    for i in range(num_samples):
-        x, y = samples[i, 0], samples[i, 1]
-        if y > polynom_3(x):
-            labels[i] = 1
+    X = np.random.uniform(low=0, high=space[0], size=(num_samples, 2))
+    Y = class_(X[:, 0], X[:, 1])
+    do_flip = np.random.binomial(n=1, p=flip_prob(*X.T))
+    Y = -(do_flip * 2 - 1) * Y
 
-        if np.abs(y - polynom_3(x)) < 0.15:
-            labels[i] = bernoulli.rvs(0.5, 0.5)
+    colormap = {-1: "red", 1: "blue"}
+    colormap = lambda Y: ['r' if i == -1 else 'b' for i in Y]
 
-    label_colors = ['r' if labels[i] == 1 else 'b' for i in range(labels)]
+    # fig, ax = plt.subplots()
+    # plt.xlim([0, 1.5])
+    # plt.ylim([0, 1.5])
+    # plt.scatter(samples[:,0], samples[:,1], c=colormap(Y))
+    # ax.set_title('Training data')
+    #
+    # xx = np.linspace(0, 1.5, 50)
+    # yy = polynom_3(xx)
+    #
+    # ax.plot(xx, yy, 'k-')
+    #
+    # plt.show()
 
-    return samples, labels, label_colors
+    return X, Y
+
