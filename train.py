be736bf33b7fd59567fce1bb3729c24c29489cae
Gowthaman Gobalasingam <gowthaman.gobalasingam@coop.ch>
2023-09-20 09:00:52 +0200

    test commit edited 2

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def resample_data():
     elif fun_name == "cos":
         X, y = sample_2D_data(5000, cos, 0.4, space)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=50)
 
     X_train = torch.tensor(X_train, dtype=torch.float).to(device)
     X_test = torch.tensor(X_test, dtype=torch.float).to(device)
